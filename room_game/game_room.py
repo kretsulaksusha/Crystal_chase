ca8b16a9c9e03d568aca8abe8c65b0b005b1f694
kretsulaksusha <kretsula.pn@ucu.edu.ua>
2023-03-12 00:46:49 +0200

    game with rooms (task 5)

---
+++ room_game/game_room.py
@@ -0,0 +1,150 @@
+"Game classes."
+
+class Room:
+    """
+    Room class.
+    """
+    def __init__(self, room: str) -> None:
+        self.room = room
+        self.description = ""
+        self.linked_rooms = {}
+        self.item = None
+        self.character = None
+
+    def __str__(self) -> str:
+        return self.room
+
+    def __repr__(self) -> str:
+        return f"Room(room={self.room})"
+
+    def set_description(self, description: str):
+        """
+        Setting description.
+        """
+        self.description = description
+
+    def link_room(self, room: object, direction: str):
+        """
+        Add linked rooms.
+        """
+        self.linked_rooms[room] = direction
+
+    def set_item(self, item: object):
+        """
+        Setting item.
+        """
+        self.item = item
+
+    def get_item(self):
+        """
+        Getting item.
+        """
+        return self.item
+
+    def set_character(self, character: object):
+        """
+        Setting character.
+        """
+        self.character = character
+
+    def get_character(self):
+        """
+        Getting character.
+        """
+        return self.character
+
+    def get_details(self):
+        """
+        Getting details.
+        """
+        details = [self.room, '--------------------', self.description,
+                   *[f"The {room} is {direct}" for room, direct in self.linked_rooms.items()]]
+        print("\n".join(details))
+
+    def move(self, command: str) -> str:
+        """
+        Move to another room.
+        """
+        return [i for i, j in self.linked_rooms.items() if j == command][0]
+
+
+class Enemy:
+    """
+    Enemy class.
+    """
+    defeated = 0
+
+    def __init__(self, name: str, description: str) -> None:
+        self.name = name
+        self.description = description
+        self.conversation = ""
+        self.weakness = None
+
+    def set_conversation(self, text: str):
+        """
+        Setting conversation.
+        """
+        self.conversation = text
+
+    def set_weakness(self, weakness: str):
+        """
+        Setting weakness.
+        """
+        self.weakness = weakness
+
+    def describe(self):
+        """
+        Description of the character.
+        """
+        if self.name and self.description:
+            print(f"{self.name} is here!\n{self.description}")
+
+    def talk(self):
+        """
+        On talk command.
+        """
+        print(f"[{self.name} says]: {self.conversation}")
+
+    def fight(self, fight_with: str):
+        """
+        Fighting.
+        """
+        if fight_with == self.weakness:
+            Enemy.defeated += 1
+            print(Enemy.defeated)
+            return True
+        return False
+
+    def get_defeated(self):
+        """
+        Getting the number of defeats.
+        """
+        return self.defeated
+
+
+class Item:
+    """
+    Item class.
+    """
+    def __init__(self, item_name: str) -> None:
+        self.item_name = item_name
+        self.description = ''
+
+    def get_name(self):
+        """
+        Getting name.
+        """
+        return self.item_name
+
+    def set_description(self, text: str):
+        """
+        Doc
+        """
+        self.description = text
+
+    def describe(self):
+        """
+        Description on a character.
+        """
+        if self.item_name and self.description:
+            print(f"The [{self.item_name}] is here - {self.description}")
---
+++ room_game/main.py
@@ -0,0 +1,98 @@
+"Game_game_room."
+
+import game_room
+
+kitchen = game_room.Room("Kitchen")
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+
+dining_hall = game_room.Room("Dining Hall")
+dining_hall.set_description("A large room with ornate golden decorations on each wall.")
+
+ballroom = game_room.Room("Ballroom")
+ballroom.set_description("""A vast room with a shiny wooden floor. Huge candlesticks \
+guard the entrance.""")
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+
+dave = game_room.Enemy("Dave", "A smelly zombie")
+dave.set_conversation("What's up, dude! I'm hungry.")
+dave.set_weakness("cheese")
+dining_hall.set_character(dave)
+
+tabitha = game_room.Enemy("Tabitha", "An enormous spider with countless eyes and furry legs.")
+tabitha.set_conversation("Sssss....I'm so bored...")
+tabitha.set_weakness("book")
+ballroom.set_character(tabitha)
+
+cheese = game_room.Item("cheese")
+cheese.set_description("A large and smelly block of cheese")
+ballroom.set_item(cheese)
+
+book = game_room.Item("book")
+book.set_description("A really good book entitled 'Knitting for dummies'")
+dining_hall.set_item(book)
+
+current_room = kitchen
+backpack = []
+
+DEAD = False
+
+while DEAD is False:
+
+    print("\n")
+    current_room.get_details()
+
+    inhabitant = current_room.get_character()
+    if inhabitant is not None:
+        inhabitant.describe()
+
+    item = current_room.get_item()
+    if item is not None:
+        item.describe()
+
+    command = input("> ")
+
+    if command in ["north", "south", "east", "west"]:
+        # Move in the given direction
+        current_room = current_room.move(command)
+    elif command == "talk":
+        # Talk to the inhabitant - check whether there is one!
+        if inhabitant is not None:
+            inhabitant.talk()
+    elif command == "fight":
+        if inhabitant is not None:
+            # Fight with the inhabitant, if there is one
+            print("What will you fight with?")
+            fight_with = input()
+
+            # Do I have this item?
+            if fight_with in backpack:
+
+                if inhabitant.fight(fight_with):
+                    # What happens if you win?
+                    print("Hooray, you won the fight!")
+                    current_room.character = None
+                    if inhabitant.get_defeated() == 2:
+                        print("Congratulations, you have vanquished the enemy horde!")
+                        DEAD = True
+                else:
+                    # What happens if you lose?
+                    print("Oh dear, you lost the fight.")
+                    print("That's the end of the game_room")
+                    DEAD = True
+            else:
+                print("You don't have a " + fight_with)
+        else:
+            print("There is no one here to fight with")
+    elif command == "take":
+        if item is not None:
+            print("You put the " + item.get_name() + " in your backpack")
+            backpack.append(item.get_name())
+            current_room.set_item(None)
+        else:
+            print("There's nothing here to take!")
+    else:
+        print("I don't know how to " + command)
